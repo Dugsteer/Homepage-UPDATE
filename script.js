cd6c137309a042cb349e05401b7f16bc10a1e6a3
Dugsteer <dugsteer@gmail.com>
2021-03-04 13:40:55 +0100

    SEO work on html

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 const menu = document.getElementById("menu");
 const sidey = document.getElementById("sidebar");
 const blue = window.matchMedia("(max-width: 800px)");
-const changePic = document.getElementById('ghostly');
+const changePic = document.getElementById("ghostly");
 const loader = document.getElementById("loaderDiv");
-const jiggly = document.getElementById('imageCard');
-const imageText = document.getElementById('imageText');
+const jiggly = document.getElementById("imageCard");
+const imageText = document.getElementById("imageText");
 
 //Show ABOUT info
-const extra = document.getElementById('info');
+const extra = document.getElementById("info");
 
-function show(){
-    info.classList.toggle('xxc');
-};
+function show() {
+  info.classList.toggle("xxc");
+}
 
-function unshow(){
-    info.classList.remove('xxc');
-};
+function unshow() {
+  info.classList.remove("xxc");
+}
 
 //Change main pic
 
 setInterval(picChanger, 5000);
 picChanger();
 
-function picChanger(){
-  changePic.innerHTML === '<img src="img/london.png" alt="">' ? changePic.innerHTML = '<img src="img/newyork.png" alt="">' :  changePic.innerHTML ='<img src="img/london.png" alt="">'
+function picChanger() {
+  changePic.innerHTML ===
+  '<img src="img/london.png" alt="London, a Great Place to Practise English" title="London, where you can put the Tools for TEFL to good use!">'
+    ? (changePic.innerHTML =
+        '<img src="img/newyork.png" alt="New York, a Super City to Practise English" title="New York, where you can learn to speak great English">')
+    : (changePic.innerHTML =
+        '<img src="img/london.png" alt="London, a Great Place to Practise English" title="London, where you can put the Tools for TEFL to good use!">');
 }
 
-const imageTextChange = ()=> {
-  setTimeout(()=>{
+const imageTextChange = () => {
+  setTimeout(() => {
     imageText.style.fontWeight = "bold";
     imageText.style.color = "#98488b";
-    imageText.innerHTML = "ACTIVATED!"
-  }, 10000)
-}
+    imageText.innerHTML = "ACTIVATED!";
+  }, 10000);
+};
 
 imageTextChange();
 
@@ -40,10 +45,10 @@ imageTextChange();
 // const countdown = document.getElementById('countdown');
 // today=new Date();
 // var cmas=new Date(today.getFullYear(), 11, 25);
-// if (today.getMonth()==11 && today.getDate()>25) 
+// if (today.getMonth()==11 && today.getDate()>25)
 // {
-// cmas.setFullYear(cmas.getFullYear()+1); 
-// }  
+// cmas.setFullYear(cmas.getFullYear()+1);
+// }
 // var one_day=1000*60*60*24;
 // countdown.innerHTML = (Math.ceil((cmas.getTime()-today.getTime())/(one_day)));
 
@@ -52,35 +57,31 @@ imageTextChange();
 setInterval(jiggleMe, 15000);
 
 jiggleMe();
- jiggleYou();
+jiggleYou();
 
-function jiggleMe(){
+function jiggleMe() {
   setTimeout(jiggleYou, 500);
- 
 }
 
-function removeJiggle(){
-  jiggly.classList.remove('jiggle');
+function removeJiggle() {
+  jiggly.classList.remove("jiggle");
 }
 
-function jiggleYou(){
-  jiggly.classList.add('jiggle');
+function jiggleYou() {
+  jiggly.classList.add("jiggle");
   setTimeout(removeJiggle, 2000);
 }
 
-
-function playHorn(){
+function playHorn() {
   const beep = new Audio("sounds/applause_clapping_w_cheers.mp3");
   beep.play();
 }
 
-jiggly.addEventListener('click', playHorn);
-
+jiggly.addEventListener("click", playHorn);
 
 //check for top of window
-function checkForTop (){
-  loader.style.top = (window.pageYOffset) + "px";
-
+function checkForTop() {
+  loader.style.top = window.pageYOffset + "px";
 }
 
 checkForTop();
@@ -89,19 +90,21 @@ function stopLoader() {
   loader.classList.add("stop");
 }
 
-function startLoader(){
+function startLoader() {
   loader.classList.remove("stop");
   setTimeout(stopLoader, 1000);
 }
 
 setTimeout(stopLoader, 1000);
 
-
-
-function myFunction(x) {if (blue.matches) {sidey.classList.remove("block");}}
-function toggler() {sidey.classList.toggle("block");}
+function myFunction(x) {
+  if (blue.matches) {
+    sidey.classList.remove("block");
+  }
+}
+function toggler() {
+  sidey.classList.toggle("block");
+}
 myFunction(blue);
 
 //GENERAL SCRIPT
-
-
