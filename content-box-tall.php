270972fedd36f60c3158271c4bd9365509ab62a6
Dugsteer <dugsteer@gmail.com>
2021-11-26 15:49:15 +0100

    Up to Contents Boxes

---
+++ content-box-tall.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ContentLong</title>
+</head>
+<body>
+     <div class="content-long">
+  <figure class="content-long__img">
+    <img src="./img/<?php echo $imgsrc?>" alt="<?php echo $title?>"/>
+  
+    </figure>
+    <div class="content-long__text">
+      <h2 class="content-title"><?php echo $title?></h2>
+      <p><?php echo $text ?></p>
+    </div>
+    <div class="content__info">
+      <h6 class="content__info-text"><?php echo $level ?></h6>
+      <h6 class="content__info-time"><?php echo $time ?></h6>
+      <a href="<?php echo $href?>"><svg class="content__info-icon">
+            <use xlink:href="sprite.svg#<?php echo $icon ?>"></use>
+          </svg>
+  </a>
+    </div>
+  </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
 <body>
 
-      <div class="social">
+    <div class="social">
         <div class="social-item">
             <a href="#" 
         onclick="
@@ -59,9 +59,8 @@
             </div>
       </div>
 
-
-
-    <div class="nav-bar" id="navbar">
+    
+      <div class="nav-bar" id="navbar">
         <a href="index.html" class="nav-bar__logo">
           <svg class="icon-header nav-bar__logo-img">
             <use xlink:href="sprite2.svg#Buho" onclick="scrollUp()">
@@ -84,213 +83,114 @@
    
     <div class="gridfiller"></div>
 
-        <div class="sidebar-container" id="sidebar">
+    <div class="sidebar-container" id="sidebar">
        <?php include "./sidebar.html" ?>
-      </div>
-
+    </div>
 
     <div class="sense">
-    </style>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6192312197226967" crossorigin="anonymous"></script>
-    <!-- mainpage_top -->
-    <ins class="adsbygoogle mainpage_top"
-        style="display:inline-block"
-        data-ad-client="ca-pub-6192312197226967"
-        data-ad-slot="3393588944"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+      </style>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6192312197226967" crossorigin="anonymous"></script>
+      <!-- mainpage_top -->
+      <ins class="adsbygoogle mainpage_top"
+          style="display:inline-block"
+          data-ad-client="ca-pub-6192312197226967"
+          data-ad-slot="3393588944"></ins>
+      <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
     </div>
 
-     
-        
-   
-    
-
-        <div class="introduction">
-          <!-- <div class="imagediv" id="imageCard"> -->
-            <!-- <h2>2021</h2> -->
-            <!-- <div class="loaderBar" id="loaderBar"></div> -->
-            <!-- <p id="imageText">loading...</p> -->
-            <!-- <img src="img/londonbus.webp" alt="London Bus"> -->
-            <!-- <div class="countdown">
-                <h3 id="countdown">10</h3>
-                <p></p>days to go!</p></div> -->
-          <!-- </div> -->
-          <div class="introduction__img" id="ghostly">
+    <div class="introduction">
+        <div class="introduction__img" id="ghostly">
             <img src="img/london.webp" alt="London, a Great Place to Practise English" title="London, where you can put the Tools for TEFL to good use!">
-          </div>
-          <div class="introduction__text">
-            <h1>ESL-Ology.com <br> TOOLS FOR TEFL
-            </h1>
+        </div>
+        <div class="introduction__text">
+            <h1>ESL-Ology.com <br> TOOLS FOR TEFL</h1>
             <p>
               Here you can find free tools for English lessons that can be used online and in the classroom. Esl-ology.com offers flashcards, memory games, hangman, grammar mazes and  more, all designed to help your classes. More material will be added regularly, so please check back to see what new tools for TEFL are available. And click around for hidden fun!
-          </p>
-
-  </div>
-
-  </div>
-
-
-
-
-
-
-  <div class="content">
-
-
-  <div class="content-6">
-    <figure class="content-6__img">
-      <a href="text-maze"><img
-        src="./img/JobsGoFish.webp"
-
-        alt="A picture of a Go Fish card game"
-        title="English jobs vocabulary cards"
-        class="content__img"
-      /></a>
-
-    </figure>
+            </p>
+        </div>
+    </div>
     
-    <div class="content-6__text">
-      <h2 class= "content-title">GO FISH CARDS</h2>
-      <p class="content-1__text">
-        Print out and play a game of <a href="text-maze">Jobs Go Fish cards</a>. 
-        Ideally played on a large screen. Or play a <a href="boardgame-player.html">Sports Board Game</a> or try a <a href="text-maze">Word Search</a>!
-      </p>
+    <div class="content">
+  
+      <?php 
+      $title= "Go Fish Cards"; 
+      $imgsrc = "JobsGoFish.webp";
+      $href = "text-maze.php";
+      $text = '<p>"Print out and play a game of <a href="text-maze.html">Jobs Go Fish cards</a>. Ideally played on a large screen. Or play a <a href="boardgame-player.html">Sports Board Game</a> or try a <a href="text-maze.html">Word Search</a> too!"</p>';
+      $level="Elementary"; 
+      $time="20 mins"; 
+      $icon="download"; 
+      include "./content-box-tall.php"; 
+      ?>
+
+      <?php 
+      $title= "Bingo Cards"; 
+      $imgsrc = "DailyRoutineBingo.webp";
+      $href = "bingo-daily-activity.php";
+      $text = '<p>"Have fun printing out random bingo cards for a variety of topics. Use the online bingo caller to call out the answers. Great for the end the class."</p>';
+      $level="Elementary"; 
+      $time="20 mins"; 
+      $icon="download"; 
+      include "./content-box-tall.php"; 
+      ?>
+
+        <?php
+      $title= "Play Any Boardgame"; 
+      $imgsrc = "JobBoardGame.webp";
+      $href = "text-maze.php";
+      $text="There are three different sets of cards in each of our matching card games. Turn up two cards and if a picture matches a word, you win them. How fast can you clear the board?";  $level="Elementary"; 
+      $time="10 mins"; 
+      $icon="online";  
+      include "./content-box-tall.php" ?>
+
+      <?php
+      $title= "Photo Flashcards"; 
+      $imgsrc = "AnimalFlash.webp";
+      $href = "bingo-daily-activity.php";
+      $text='<p>A great way to present or revise <a href="flashcard-animals.html">vocabulary sets </a>with a class, either with traditional flashcards or modern "hover cards", these are available both with images and with photos.</p>';
+      $time="15 mins"; 
+      $icon="online";  
+      include "./content-box-short.php" ?>
+
+      <?php 
+      $title= "Grammar Mazes"; 
+      $imgsrc = "GrammarMaze.webp";
+      $href = "to-be-maze.html";
+      $text = '<p>All of the <a href="questions-maze.html">grammar mazes </a>help student awareness of grammar mistakes. The aim is to have a clean run all the way through!"</p>'; 
+      $level="Beginner"; 
+      $time="15 mins"; 
+      $icon="online"; 
+      include "./content-box-short.php"; 
+      ?>
+
+      <?php 
+      $title= "Memory Games"; 
+      $imgsrc = "SportMemory.webp";
+      $href = "matching-sports.html";
+      $text = '<p>There are three different sets of cards in each of our matching card games. Turn up two cards and if a picture matches a word, you win them. How fast can you clear the board?</p>'; 
+      $level="Elementary"; 
+      $time="20mins"; 
+      $icon="online"; 
+      include "./content-box-short.php"; 
+      ?>
+
     </div>
-  </div>
 
-  <div class="content-4">
-    <figure class="content-4__img">
-      <a href="bingo-daily-activity.html"><img
-        src="./img/DailyRoutineBingo.webp"
-        alt="A picture of bingo cards "
-        title="Practise English Vehicles Vocabulary with a Text Maze"
-        class="content__img"
-      /></a>
-    </figure>
-    
- <div class="content-6__text">
-      <h2 class= "content-title">Bingo Cards</h2>
-      <p class="content-1__text">
-  Have fun printing out random bingo cards for a variety of topics. Use the online bingo caller to call out the answers. Great to end the class.      </p>
+    <div class="sense-bottom">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- Vertical ad -->
+      <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-6192312197226967"
+        data-ad-slot="3073690633"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
     </div>
-  </div>
-
-
-
- 
-
- <div class="content-5">
-  <figure class="content-5__img">
-    <a href="boardgame-player.html"><img src="./img/JobBoardGame.webp" alt="Play any boardgame online" title="Practise English Grammar with Great Online Boardgames" class="content__img"/>
-    </a>
-    </figure>
-    <div class="content-5__text">
-      <h2 class="content-title">Play Any Boardgame</h2>
-      <p class="content-1__text">
-        Load any ESL teaching boardgame into a window so that students can play online or onscreen. With draggable pieces and die. 
-      </p>
-    </div>
-    <div class="content__info">
-      <h6 class="content__info-text">Elementary</h6>
-      <h6 class="content__info-time">10mins</h6>
-      <svg class="content__info-icon">
-            <use xlink:href="sprite.svg#downarrow"></use>
-          </svg>
-
-    </div>
-  </div>
-
-
-
-  <div class="content-7">
-  <figure class="content-7__img">
-   <a href="flashcard-animals.html"> <img
-    src="img/AnimalFlash.webp"
-      alt="An English Vocabulary Flashcard featuring a Clownfish"
-      title="Flashcards are a Great Tool for TEFL for Learning Vocabulary"
-      class="content__img"
-    /></a>
-  </figure>
-
-  <div class="content-6__text">
-    <h2 class= "content-title">Photo Flashcards</h2>
-    <p class="content-1__text">
-      A great way to present or revise <a href="flashcard-animals.html">vocabulary sets </a>with a class, either with traditional flashcards or modern 'hover cards', these are available both with images and with photos.
-    </p>
-  </div>
-  </div>
-
-
-  <div class="sense1">
-</style>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6192312197226967" crossorigin="anonymous"></script>
-<!-- mainpage_top -->
-<ins class="adsbygoogle mainpage_top"
-     style="display:inline-block"
-     data-ad-client="ca-pub-6192312197226967"
-     data-ad-slot="3393588944"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-
-<div class="content-8">
-  <figure class="content-8__img">
-    <a href="to-be-maze.html"> <img
-      src="/img/GrammarMaze.webp"
-      alt="A Classic ESL-ology Grammar Maze"
-      title="Grammar Mazes Help to Make English Learning Stick!"
-      class="content__img"
-    /></a>
-    </figure>
-   
-    <div class="content-8__text">
-     <h2 class= "content-title">Grammar Mazes</h2>
-      <p class="content-1__text">
-        All of the <a href="questions-maze.html">grammar mazes </a>help student awareness of grammar mistakes. The aim is to have a clean run all the way through!
-      </p>
-    </div>
-</div>
-
-<div class="content-9">
-  <figure class="content-9__img">
-<a href="matching-sports.html"><img src="img/SportMemory.webp" alt="An English Vocabulary Food Matching Game" title="Tools for TEFL - Matching Games Help Reinforce English Learning"></a>
-  </figure>
-  <div class="content-8__text">
-    <h2 class= "content-title">Card Matching Games</h2>
-    <p class="content-1__text">
-      There are three different sets of cards in each of our matching card games. Turn up two cards and if a picture matches a word, you win them. How fast can you clear the board?
-    </p>
-  </div>
-    <div class="content__info">
-        <h6 class="content__info-text">Elementary</h6>
-        <h6 class="content__info-time">10mins</h6>
-        <svg class="content__info-icon">
-              <use xlink:href="sprite.svg#laptop"></use>
-            </svg>
-      </div>
-</div>
-
-
-
-
-</div>
-
-<div class="sense-bottom">
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Vertical ad -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-6192312197226967"
-     data-ad-slot="3073690633"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
 
       <div class="links">
         <h2 class="links__text">
@@ -319,7 +219,7 @@
       
     <footer class="footer">
       <div class="footer__copyright" id="footer">
-        <p>© 2021 Copyright Esl-ology.com & copyright owners.</p>
+        <p>©<?php echo date("Y"); ?> Copyright Esl-ology.com & copyright owners.</p>
         <p>Image credits <a href="homepage-credits.html" style="color: white; text-decoration: none;">here</a>. Thanks,guys!</p>
   </div>
     </footer>
